8408c962a969028fc4f8643fcbacbeea5db7786e
Aarav Gupta <25aargupta@asfg.edu.mx>
2026-02-10 11:19:50 -0600

    restored previous ui changes for test

---
+++ onboarding_backend/python/__init__.py
@@ -0,0 +1,5 @@
+#what is __init__.py used for? 
+#It is used to mark a directory as a Python package and can also be used to initialize package-level variables or import submodules.
+
+
+#example code 
